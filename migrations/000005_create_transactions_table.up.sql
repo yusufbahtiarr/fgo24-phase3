ccd8421f253b5de434e8c8c6fbc5ab5327acf033
yusufbahtiarr <yusufbdev@gmail.com>
2025-07-25 01:24:53 +0700

    chore(migration): remove unit_price, total_price, and transaction_date columns from transactions table

--- migrations/000005_create_transactions_table.up.sql
+++ migrations/000005_create_transactions_table.up.sql
@@ -3,9 +3,6 @@ CREATE TABLE transactions (
     transaction_type transaction_type_enum NOT NULL,
     product_id INT NOT NULL REFERENCES products(id) ON DELETE CASCADE ON UPDATE CASCADE,
     quantity INT NOT NULL,
-    unit_price DECIMAL(12,2) NOT NULL,
-    total_price DECIMAL(12,2) NOT NULL,
-    transaction_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE,
     created_at TIMESTAMP DEFAULT NOW(),
     updated_at TIMESTAMP DEFAULT NOW()
