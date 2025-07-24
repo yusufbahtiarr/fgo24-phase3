84751c7f9bee40c1e9e98977e51d1ed14b1a9cf3
yusufbahtiarr <yusufbdev@gmail.com>
2025-07-25 00:39:54 +0700

    feat: add password verification using Argon2 hashing

--- utils/password.go
+++ utils/password.go
@@ -12,3 +12,10 @@ func HashPassword(password string) string {
 	encoded := base64.RawStdEncoding.EncodeToString(hash)
 	return encoded
 }
+
+func VerifyPassword(hashedPassword, inputPassword string) bool {
+	salt := []byte("static_salt")
+	inputHash := argon2.IDKey([]byte(inputPassword), salt, 1, 64*1024, 4, 32)
+	encoded := base64.RawStdEncoding.EncodeToString(inputHash)
+	return encoded == hashedPassword
+}
